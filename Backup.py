3feb59dc3d5ce4d4ba13cc97fcf4071afe3b5dab
FreakyBigFoot <mustbaj@gmail.com>
2016-11-25 03:03:45 -0800

    Creation

---
+++ Backup.py
@@ -0,0 +1,33 @@
+# Include the Dropbox SDK
+import dropbox
+import os
+
+dbx = dropbox.Dropbox('KEY')
+
+# x = dbx.users_get_current_account()
+
+# print x
+
+
+client = dropbox.client.DropboxClient('KEY')
+# f = open('/home/pi/RetroPie/roms/gba/', 'rb')
+# response = client.put_file('/test.txt', f)
+# print 'uploaded: ', response
+
+# folder_metadata = client.metadata('/')
+# print 'metadata: ', folder_metadata
+
+# f, metadata = client.get_file_and_metadata('/magnum-opus.txt')
+# out = open('magnum-opus.txt', 'wb')
+# out.write(f.read())
+# out.close()
+# print metadata
+
+for root, dirs, files in os.walk("/home/pi/RetroPie/roms"):
+    for name in files:
+        if name.endswith((".state", ".htm")):
+            print name
+			
+			
+			
+response = client.put_file('dir/name', f)
